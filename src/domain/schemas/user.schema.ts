930ca7cdb00d2da17a0b89648ef61416ad7f398b
programmingwithnavilla <navidbarsalari@gmail.com>
2025-06-24 22:34:44 +0330

    add user schema

--- src/domain/schemas/user.schema.ts
+++ src/domain/schemas/user.schema.ts
@@ -1,1 +1,11 @@
 import { z } from "zod";
+
+export const UserSchema = z.object({
+  id: z.string().uuid(),
+  name: z.string().min(2).max(50),
+  isDelete: z.boolean().optional(),
+  createdAt: z.date().optional(),
+  updateAt: z.date().optional(),
+  deletedAt: z.date().optional(),
+  version: z.number(),
+});
