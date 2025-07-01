a47a48e6e2c6ff9263b5e87b621618858958e61c
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-01 22:35:46 +0330

    fix finbById in UserReadRepository

--- src/infrastructure/repositories/mongo/UserReadRepository.ts
+++ src/infrastructure/repositories/mongo/UserReadRepository.ts
@@ -15,8 +15,7 @@ export class UserReadRepository implements IRead<User> {
   }
   async findById(id: string): Promise<User | null> {
     const data = await UserMongoModel.findOne({ id }).lean();
-    if (!data) return null;
-
+    if (!data || !data.name || !data.email) return null;
     return new User(data.name, data.email);
   }
 
