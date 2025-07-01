168f1a429926c9e603d9868aea241663788db404
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-01 16:10:37 +0330

    add postgress data source

---
+++ src/infrastructure/database/postgres-data-source.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+export const PostgresDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DATABASE_HOST || "localhost",
+  port: process.env.DATABASE_PORT ? Number(process.env.DATABASE_PORT) : 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  synchronize: true,
+  logging: false,
+  entities: [UserEntity],
+});
