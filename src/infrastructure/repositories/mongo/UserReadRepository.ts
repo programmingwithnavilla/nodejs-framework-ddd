ab861c7efcbc84df544a2112cc299198ed3cf38e
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-01 16:08:22 +0330

    implemented user Read repository

---
+++ src/infrastructure/repositories/mongo/UserReadRepository.ts
@@ -0,0 +1,27 @@
+// src/infrastructure/repositories/mongo/UserReadRepository.ts
+
+import { IRead } from "domain/interfaces/IRead.interface";
+import { UserMongoModel } from "./UserMongoSchema";
+import { injectable } from "tsyringe";
+import { User } from "domain/entities/user.entity";
+
+@injectable()
+export class UserReadRepository implements IRead<User> {
+  findByPagination(
+    page: number,
+    limit: number
+  ): Promise<{ data: User[]; total: number }> {
+    throw new Error("Method not implemented.");
+  }
+  async findById(id: string): Promise<User | null> {
+    const data = await UserMongoModel.findOne({ id }).lean();
+    if (!data) return null;
+
+    return new User(data.name, data.email);
+  }
+
+  async findAll(): Promise<User[]> {
+    const list = await UserMongoModel.find().lean();
+    return list.map((data) => new User(data.name, data.email));
+  }
+}
