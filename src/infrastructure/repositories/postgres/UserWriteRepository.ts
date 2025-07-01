a15f9c374d52cb7f86ba79ce66a3199d80d479a6
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-01 16:08:02 +0330

    mock function of user write repository

--- src/infrastructure/repositories/postgres/UserWriteRepository.ts
+++ src/infrastructure/repositories/postgres/UserWriteRepository.ts
@@ -7,7 +7,27 @@ import { PostgresDataSource } from "infrastructure/database/postgres-data-source
 @injectable()
 export class UserWriteRepository implements IWrite<User> {
   private repo = PostgresDataSource.getRepository(UserEntity);
-
+  softDelete(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  restore(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  bulkCreate(entities: User[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  bulkUpdate(entities: User[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  bulkDelete(ids: string[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  bulkSoftDelete(ids: string[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  bulkRestore(ids: string[]): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
   async create(user: User): Promise<void> {
     const entity = this.repo.create({
       id: user.id,
