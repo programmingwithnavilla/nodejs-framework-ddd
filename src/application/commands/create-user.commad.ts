7c4f4ad8d11684d23cdfad3cf1a1a3beea32c49f
programmingwithnavilla <navidbarsalari@gmail.com>
2025-07-05 15:32:24 +0330

    rename user command

--- src/application/commands/create-user.commad.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from "./command.interface";
-
-export class CreateUserCommand implements Command {
-  constructor(
-    public readonly id: string,
-    public readonly name: string,
-    public readonly email: string
-  ) {}
-}
